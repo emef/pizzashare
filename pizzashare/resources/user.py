32e43b72652e7962b82a2a9d20db0befa00f0de1
matt forbes <matt@freshplum.com>
2014-07-06 12:56:36 -0700

    tidy

--- pizzashare/resources/user.py
+++ pizzashare/resources/user.py
@@ -8,28 +8,26 @@ parser = reqparse.RequestParser()
 parser.add_argument('uuid', type=str, help='uuid=device UUID', required=True)
 parser.add_argument('name', type=str, help='name=name of user')
 
-user_fields = {
+fields = {
     'uuid': fields.String,
     'name': fields.String,
 }
 
 
 class UserResource(restful.Resource):
-    @marshal_with(user_fields)
+    @marshal_with(fields)
     def get(self):
         args = parser.parse_args()
         uuid = args['uuid']
-        if not uuid:
-            return None
-
         session = db.Session()
         query = session.query(User).filter(User.uuid == uuid)
+
         try:
             return query.one()
         except exc.NoResultFound:
             return None
 
-    @marshal_with(user_fields)
+    @marshal_with(fields)
     def put(self):
         args = parser.parse_args()
         uuid, name = args['uuid'], args['name']
